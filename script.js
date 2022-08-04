f3bf611bfafd26bf26574c61bf96b60df8ee1d58
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-08-04 01:34:28 -0400

    CSS change to float image right

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.write("Hi "  + username + " Your adventure awaits! ");
 
     let q1 = prompt("Hello " + username +" Would you like to explore the uknown?");
     
-    if (q1 == "yes" || q1 == "YES" || q1 == "Yes"){
+    if (q1 == "yes" || q1 == "YES" || q1 == "Yes" || q1 == "y"){
         alert("Then get ready for your your greatest adventure yet!");
     }
     else{
