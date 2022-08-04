b81ca772a0631d6746c60c5564fe470b34a2ffc8
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-08-03 23:57:49 -0400

    Add functions in jS

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 const username = prompt("What is your name?");
 document.write("Hi "  + username + " Your adventure awaits! ");
 
+function adventure(){
+    let q1 = prompt("Would you like to explore the uknown?");
+    
+    if (q1 == "yes" || q1 == "YES" || q1 == "Yes"){
+        alert("Then get ready for your your greates adventure yet!");
+    }
+    else{
+        alert("hmm.. There's no way!Let me ask again");
+    }
+}
+
+function wrong_answer(){
+    let q2 = confirm("Would you like to explore the uknown?");
+
+    if(q1){
+        alert("I knew it! Get ready for your next adventure!");
+    }else{
+        alert("Then get ready to change your mind!");
+    }
+}
