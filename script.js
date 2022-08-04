c90168645214f3f5c91bb0e0c36901995ec91355
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-08-04 16:02:41 -0400

    Add functions into HTML

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+function name(){
 const username = prompt("What is your name?");
-document.write("Hi "  + username + "," + " Your adventure awaits! ");
-
+message = "Hi "  + username + "," + " Your adventure awaits! ";
+document.write(message);
 
     let q1 = prompt("Hello " + username +" Would you like to explore the uknown?");
-    
     if (q1 == "yes" || q1 == "YES" || q1 == "Yes" || q1 == "y"){
         alert("Then get ready for your your greatest adventure yet!");
     }
     else{
         alert("hmm... There's no way! Let me change your mind!");
+
+        next_response(){
+
+        }
     }
+}
+
+function park()
+{
+    let name = document.querySelector('#name').value;
+    alert(name + ' is one of my favorites too!');
+}
+
 
 
