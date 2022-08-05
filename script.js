caa59b3430770b84c483b68ca71be6006a2a50ef
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-08-04 23:14:46 -0400

    Started loop for picture

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ function getPark(){
     let name = document.querySelector('#name').value;
     alert(name + ' is one of my favorites too!');
 }
+
+// function take_Picture(){
+
+//     let pictures = 0;
+//     pictures = prompt("How many arches are in Arches National Park");
+//     for (let i = 1; i <= pictures; i++){
+//         img.src = "/Users/maximovincentemejia/projects/courses/code_102/camping-website/IMG_3951.png";
+//         document.write("here is " + i + " pictures");
+//     }
+// }
+
+
